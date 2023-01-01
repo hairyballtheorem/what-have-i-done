ddc605710f0e2b1074436e6ace2076b80dc936ab
Mike Okslong <69575053+thatOneArchUser@users.noreply.github.com>
2023-01-01 13:54:16 +0200

    Add files via upload

---
+++ src/boot/boot.asm
@@ -0,0 +1,29 @@
+MEMALIGN equ 1 << 0
+MEMINFO  equ 1 << 1
+
+MAGIC    equ 0x1BADB002
+FLAGS    equ MEMALIGN | MEMINFO
+CHECKSUM equ -(MAGIC + FLAGS)
+
+extern εκκίνηση
+
+section .multiboot
+align 4
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
+
+section .bss
+align 16
+    stack_bottom:
+    resb 16384
+    stack_top:
+
+section .text
+global _start
+_start:
+    mov esp, stack_top
+
+    cli
+    call εκκίνηση
+    jmp $
---
+++ src/include/common.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#define θετικός unsigned
+#define στατικός static
+#define επέστρεψε return
+#define ακαίρεος int
+#define χαρακτήρας char
+#define μακρύς long
+#define μικρός short
+#define κενός void
+#define σταθερός const
+#define δεκαδικός float
+#define όρισε_κύρια int main
+#define αν if
+#define αλλιώς else
+#define γιά for
+#define ενώ while
+#define διακοπή break
+#define αλλαγή switch
+#define περίπτωση case
+#define προεπιλογή default
+#define εξωτερικό extern
+#define αληθής 1 //true
+#define ψευδής 0 //false
+#define δομή struct
+#define __χαρακτηριστικό__ __attribute__
+#define πακεταρισμένο packed //not the best translation
+#define μέγεθος sizeof
+#define πτητικός volatile
+#define πτητική_εντολή asm volatile
